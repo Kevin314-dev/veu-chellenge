0571a460d45ef84e6116a618f8b18210fa84269b
kevin314-dev <cherloancecannaoan@gmail.com>
2024-07-21 20:51:43 -0700

    complete

--- src/components/RecipientsDisplay.vue
+++ src/components/RecipientsDisplay.vue
@@ -1,33 +1,14 @@
 
 <script setup lang="ts">
 
-import { method } from 'lodash-es';
+
 import RecipientsBadge from './RecipientsBadge.vue'
  import { ref, onMounted , onUnmounted} from 'vue';
 
       // Create a reactive reference to store the window width
       const windowWidth = ref(window.innerWidth);
-      const use_space = ref((window.innerWidth-208)/2-76);
-      // Function to update the window width
-      const updateWindowWidth = () => {
-        windowWidth.value = window.innerWidth;
-        use_space.value = (windowWidth.value-208)/2-76;
-        
-      };
-      // const getDispalyemail = () => {
-
-      // }
-      // Add event listeners when the component is mounted
-      onMounted(() => {
-        window.addEventListener('resize', updateWindowWidth);
-        // window.addEventListener('resize', getDispalyemail);
-      });
-
-      // Remove event listeners when the component is unmounted
-      onUnmounted(() => {
-        window.removeEventListener('resize', updateWindowWidth);
-      });
- 
+      const use_space = ref(((window.innerWidth-208)/2-76)/8);
+      
       
       const props = defineProps<{
         recipients: string[]
@@ -38,40 +19,71 @@ import RecipientsBadge from './RecipientsBadge.vue'
      // const dataDisplay = recipientsObject[0];
       let tooltipDisplay :string = recipientsObject[0];
       let num: number = 0;
-      let count:number = recipientsObject[0].length;
-      let email_num :number = recipientsObject.length - 1;
-      let dataDisplayMulti:string = recipientsObject[0];
 
-    for( num = 1; num < recipientsObject.length; num++)
+
+       for( num = 1; num < recipientsObject.length; num++)
           {    
               tooltipDisplay +=", " + (recipientsObject[num]);
-    }
-
-    if(email_num > 0 )
-      {
-        for( num = 1; num < recipientsObject.length; num++)
-          {
-            if(count+recipientsObject[num].length < use_space.value/8)
-            {
-              count =count + recipientsObject[num].length;
-              dataDisplayMulti +=", " + (recipientsObject[num]);
-              email_num -= 1;
-            }
-            else{
-              
-            }
-            
           }
-          //dataDisplayMulti += count; 
-          if( email_num != 0 )
-            { 
-              dataDisplayMulti +=", ...";
+
+      let dataDisplayMulti = ref(recipientsObject[0]);
+      let email_num  = ref(0);
+      let count:number = recipientsObject[0].length;
+      
+      function getDispalyemail(){
+        console.log("create")
+        email_num.value  = recipientsObject.length - 1;
+          
+          if(email_num.value > 0 ) 
+            {
+              for( num = 1; num < recipientsObject.length; num++ )
+                {
+                  if((count + recipientsObject[num].length)  < (use_space.value-6))
+                  {
+                    count = count + recipientsObject[num].length;
+                    dataDisplayMulti.value += ", " + (recipientsObject[num]);
+                    email_num.value -= 1;
+                  }
+                  else{
+                    break;
+                  }
+                  
+                }
+                //dataDisplayMulti += count; 
+                if( email_num.value != 0 )
+                  { 
+                    dataDisplayMulti.value +=", ...";
+                  }
+            } 
+          else{
+              dataDisplayMulti.value = recipientsObject[0];
+              email_num.value = 0;
             }
-      } 
-      else{
-        dataDisplayMulti = recipientsObject[0];
-        email_num = 0;
       }
+      getDispalyemail();
+
+      // Function to update the window width
+
+      const updateWindowWidth = () => {
+        if( windowWidth.value !=window.innerWidth)
+            windowWidth.value = window.innerWidth;
+            use_space.value = ((windowWidth.value-208)/2-76)/8;       
+            window.location.reload();
+
+      };
+
+      // Add event listeners when the component is mounted
+      onMounted(() => {
+        window.addEventListener('resize', updateWindowWidth);
+       
+      });
+
+      // Remove event listeners when the component is unmounted
+      onUnmounted(() => {
+        window.removeEventListener('resize', updateWindowWidth);
+       
+      });
+      
 
 
 </script>
@@ -81,10 +93,10 @@ import RecipientsBadge from './RecipientsBadge.vue'
   <div id="app1">
     <div class="badge-div" ref="cell">
       <!-- <button @click="changeVal">get</button> -->
-        <span v-if="email_num <= 1 " class="ellipsis">{{ use_space }}</span>
+        <span v-if="email_num <= 1 " class="ellipsis">{{ dataDisplayMulti }}</span>
      
       
-        <span v-else-if="email_num > 1" class="ellipsis">{{ windowWidth }}</span>
+        <span v-else-if="email_num > 1" class="ellipsis">{{ dataDisplayMulti }}</span>
        
       
       <div class="badge-left" v-if="email_num >= 1" @mouseover="tp_show = false" @mouseleave=" tp_show = true">
